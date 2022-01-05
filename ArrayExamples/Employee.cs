258084bbb975678ec5f0ce67b7be08b3cc8512ea
nbhir <nbhir@DESKTOP-RKMH2OS>
2022-01-05 16:00:00 +0530

    Add project files.

---
+++ ArrayExamples/Employee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExamples
+{
+    class Employee
+    {
+       public int empid;
+       public string ename;
+       public string location;
+        public static int count;
+
+        static Employee()
+        {
+            Console.WriteLine("Static Constructor");
+            count = 0;
+        }
+        public Employee()
+        {
+            Console.WriteLine("Enter employee id");
+            empid = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter employee name");
+            ename = Console.ReadLine();
+            Console.WriteLine("Enter work location");
+            location = Console.ReadLine();
+            count++;
+        }
+        public Employee(int id, string ename,string location)
+        {
+            empid = id;
+            this.ename = ename;
+            this.location = location;
+        }
+       
+        //public void acceptData()
+        //{
+        //    Console.WriteLine("Enter employee id");
+        //    empid = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("Enter employee name");
+        //    ename = Console.ReadLine();
+        //    Console.WriteLine("Enter employee id");
+        //    location = Console.ReadLine();
+        //}
+        public void display()
+        {
+            Console.WriteLine(empid);
+            Console.WriteLine(ename);
+            Console.WriteLine(location);
+        }
+        
+    }
+}
---
+++ ArrayExamples/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee();
+            e1.display();
+            Employee e2 = new Employee();
+            e2.display();
+            Employee e3 = new Employee();
+            e3.display();
+            Employee e4 = new Employee(678, "Prasad", "Pune");
+            Console.WriteLine("Total No. Of Employees:"+Employee.count);
+           
+            //int[] nums = new int[5];
+            //int[] nums1 = new int[] { 23, 45, 77, 89 };
+            ////for (int i=0;i<5;i++)
+            //{
+            //    Console.WriteLine("Enter element "+i);
+            //    nums[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Console.WriteLine("Array Data");
+            //for (int i = 0; i < 5; i++)
+            //{
+
+            //    Console.WriteLine(nums[i]);  
+            //}
+            //int[][] jArr = new int[3][];
+            //jArr[0] = new int[4];
+            //jArr[1] = new int[3];
+            //jArr[2] = new int[5];
+            //for(int i=0;i<3; i++)
+            //{
+            //    for(int j=0;j<jArr[i].Length;j++)
+            //    {
+            //        Console.WriteLine("Enter element {0}{1}",i,j);
+            //        jArr[i][j] = Convert.ToInt32(Console.ReadLine());
+            //    }
+            //}
+            //Console.WriteLine("Jagged Array Data");
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < jArr[i].Length; j++)
+            //    {
+            //        Console.Write(jArr[i][j]+" ");
+
+            //    }
+            //    Console.WriteLine();
+            //}
+
+        }
+    }
+}
